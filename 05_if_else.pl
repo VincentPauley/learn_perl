a241a28d7490a83a1a00ce142c538eda657370cd
Vincent Pauley <vinniepauley@gmail.com>
2016-06-15 20:31:42 -0500

    more conditionals and while loop

---
+++ 05_if_else.pl
@@ -0,0 +1,8 @@
+# if else, same as js pretty much
+my $carMiles = 150000;
+
+if($carMiles < 40000) {
+  print "still a newer car";
+} else {
+  print "doin okay, check dat oil";
+}
---
+++ 06_while_loops.pl
@@ -0,0 +1,12 @@
+# while loops
+
+$a = 0;
+
+# extremely simple syntax
+while($a < 20) {
+  print "on pass: ".$a."\n";
+  # increment $a
+  $a = $a + 1;
+}
+
+# while the condition is true, 
